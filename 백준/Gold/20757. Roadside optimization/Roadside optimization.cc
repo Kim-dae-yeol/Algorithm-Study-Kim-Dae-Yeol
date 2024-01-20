576a12a360c041c0eff056df9690d5d01662e4f4
김대열 <115692722+Kim-dae-yeol@users.noreply.github.com>
2024-01-21 01:17:55 +0900

    [Gold V] Title: Roadside optimization, Time: 8 ms, Memory: 2184 KB -BaekjoonHub

--- 백준/Gold/20757. Roadside optimization/Roadside optimization.cc
+++ 백준/Gold/20757. Roadside optimization/Roadside optimization.cc
@@ -1,59 +1,51 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+// Online IDE - Code Editor, Compiler, Interpreter
+
+#include<bits/stdc++.h>
 
 using namespace std;
+int p[205];
+int adj[205][205];
 
-vector<int> root;
-
-int find(int x) {
-    if (root[x] != x)
-        root[x] = find(root[x]);
-    return root[x];
+int find(int u){
+    if(p[u] != u)
+        p[u] = find(p[u]);
+    return p[u];
 }
 
-bool unite(int x, int y) {
-    x = find(x);
-    y = find(y);
-
-    if (x == y)
-        return false;
-
-    if (x < y)
-        root[y] = x;
-    else
-        root[x] = y;
-
+bool is_diff_group(int u, int v){
+    u = find(u); v = find(v);
+    if(u == v) return false;
+    
+    if(p[u] < p[v]) p[v] = u;
+    else p[u] = v;
     return true;
 }
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-
-    int n, cnt, x, y;
-    int t;
+int main()
+{
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ios::sync_with_stdio(false);
+    int n, t;
     cin >> t;
-
-    while (t-- > 0) {
+    while(t--){
         cin >> n;
-        cnt = 0;
-        root.resize(n);
-
-        for (int i = 0; i < n; i++) {
-            root[i] = i;
-        }
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                cin >> x;
-                if (x == 1 && unite(i, j))
-                    cnt++;
+        
+        for(int i = 1; i <= n; i++){
+            p[i] = i;
+            for(int j = 1; j <= n; j++){
+                cin >> adj[i][j];
             }
         }
-
+        
+        int cnt = 0;
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n; j++){
+                if(adj[i][j] && is_diff_group(i, j)) {
+                    cnt ++;
+                }
+            }
+        }
         cout << cnt << "\n";
     }
-
-    return 0;
 }
