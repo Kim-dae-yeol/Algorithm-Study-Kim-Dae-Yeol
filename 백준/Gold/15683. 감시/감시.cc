c57ce86fe4447e287b07b97023c900dd8dc2fd43
김대열 <115692722+Kim-dae-yeol@users.noreply.github.com>
2024-01-23 13:26:49 +0900

    [Gold IV] Title: 감시, Time: 76 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.cc
+++ 백준/Gold/15683. 감시/감시.cc
@@ -5,19 +5,20 @@
 #include<vector>
 #include<queue>
 #include<tuple>
-#include<cassert>
 
 using namespace std;
 int invisible = 0;
 int n, m;
-int map[11][11];
-bool light[11][11];
-bool vis[4][11][11];
+int map[9][9];
+bool vis[4][9][9];
+bool light[9][9];
 int dx[4]{0, 1, 0, -1};
 int dy[4]{-1, 0, 1, 0};
-queue<tuple<int, int, int>> q;
+
 int bfs(vector<tuple<int, int, int>>& cam){
-    for(auto e : cam){
+    queue<tuple<int, int, int>> q; // curPos, cur dir
+    
+    for(const auto& e : cam){
         int r,c,cnum,dir;
         tie(r,c,dir) = e;
         cnum = map[r][c];
@@ -70,19 +71,15 @@ int bfs(vector<tuple<int, int, int>>& cam){
     return cnt;
 }
 
-int dfs(vector<tuple<int, int, int>> cam, int idx){
+int dfs(vector<tuple<int, int, int>>& cam, int idx){
     // cout << "dfs : "<< idx << "\n";
     
     if(idx >= cam.size()) {
         // cout << "bfs" << "\n";
-        while(q.empty() == false) q.pop();
-        for(int i = 0 ; i < n; i++){
-            fill(light[i], light[i] + m , false);
-        }
-        
         for(int i = 0; i < 4; i++){
             for(int j = 0 ; j < n; j++){
-                fill(vis[i][j], vis[i][j] + m  , false);
+                fill(light[j], light[j] + m + 1 , false);
+                fill(vis[i][j], vis[i][j] + m + 1 , false);
             }
         }
         return invisible - bfs(cam);
@@ -98,7 +95,7 @@ int dfs(vector<tuple<int, int, int>> cam, int idx){
             ret = min(ret, dfs(cam, idx + 1));
             break;
         }
-        std::get<2>(cam[idx]) = i;
+        get<2>(cur) = i;
         ret = min(ret, dfs(cam, idx + 1));
     }
     
@@ -123,8 +120,6 @@ int main()
             }
         }
     }
-    int result = dfs(cam, 0);
-    assert(result >= 0);
-    cout << result;
+    cout << dfs(cam, 0);
     return 0;
 }
