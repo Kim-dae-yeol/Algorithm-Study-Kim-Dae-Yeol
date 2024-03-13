6253feadb108507f5ba74a07ed212d27da784237
김대열 <115692722+Kim-dae-yeol@users.noreply.github.com>
2024-03-13 09:49:45 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.cc
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int k,q,l,kn, b, p;
+    cin >> k >> q >> l >> kn >> b >> p;
+    cout << 1 - k << " " << 1 - q << " " << 2 - l << " " << 2 - kn << " " << 2 - b << " " << 8 - p;
+    return 0;
+}
